9c13ef12dff547a88485d683c2876546798b8740
Miłosz Gajda <gajda.milosz@gmail.com>
2025-09-26 15:54:18 +0200

    setting GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // https://ucilasmana.medium.com/deploying-a-static-vite-app-on-github-pages-with-github-actions-03a67bb9ac4e
+  base: '/zadanie-rekrutacyjne_26092025/',
   plugins: [
     tailwindcss(),
   ],
